78708683878b5add5231148d0767bcea5905c2a4
Blessing <96212794+Bab-exe@users.noreply.github.com>
2022-09-08 08:56:03 -0400

    Add files via upload
    Lab 1 code

--- Labs/Lab 1/lab1.cpp
+++ Labs/Lab 1/lab1.cpp
@@ -1,14 +1,19 @@
+/*
+	Blessing Abumere
+	9/7/2022 3:20pm
+	GEN COMPUTER SCI (COSC175006)-1224
+*/
 #include <iostream>
 using namespace std;
 int main()
 {
 	float num1; 
-	float num2;                   
+	float num2; 
 	float avg; 
 
 	cout << "Enter two numbers" << endl; 
 	cin >> num1 >> num2; 
-	avg = (num1 + num2)/2; 
+	avg = ( num1 + num2) /2; 
 	cout << "The average is " << avg;
 
        return 0;
