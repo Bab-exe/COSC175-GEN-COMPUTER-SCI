9ded29f4a3ced1a3011347ce329191f0f7341dbd
Blessing <96212794+Bab-exe@users.noreply.github.com>
2022-09-28 11:41:57 -0400

    Added Lab 4

---
+++ Lab 4/lab4.cpp
@@ -0,0 +1,33 @@
+/*
+    Blessing Abumere
+    9/28/2022
+    GEN COMPUTER SCI (COSC175006)-1224
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int testScore;
+
+    cout << "Enter test score" << endl;
+        cin >> testScore;
+            if (testScore > 100 || testScore < 0){
+                cout << "Invalid Test Score" << endl;
+                return 0;
+            }
+
+    if (testScore >= 90)
+        cout << "Your grade is A" << endl;
+    else if (testScore >= 80)
+        cout << "Your grade is B" << endl;
+    else if (testScore >= 70)
+        cout << "Your grade is C" << endl;
+    else if (testScore >= 60)
+        cout << "Your grade is D" << endl;
+    else 
+        cout << "Your grade is F" << endl;
+
+    return 0;
+}
