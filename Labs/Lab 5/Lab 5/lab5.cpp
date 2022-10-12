0cde6dab74bf99d402bd986b6bd9a0a1297ebf30
Blessing <96212794+Bab-exe@users.noreply.github.com>
2022-10-12 10:41:05 -0400

    Lab 5 Final
    The final product of lab 5

---
+++ Labs/Lab 5/Lab 5/lab5.cpp
@@ -0,0 +1,33 @@
+/* 
+    Blessing Abumere
+    10/5/2022
+    COSC 175
+*/
+
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+    //input vars 
+    float length; //room length
+    float width; //room width
+
+    //output vars
+    float perimeter; //perimeter = 2 x (length + width)
+    float area; //area = length x width
+
+    cout << "\nEnter Length Of Room: " ;
+        cin >> length;    
+        
+    cout << "Enter Width Of Room: ";
+        cin >> width;
+    
+    perimeter = 2 * (length + width); //perimeter calc
+    area = length * width; //area calc 
+
+    cout << "\nThe Perimeter of a " << length << "ft long by " << width << "ft wide room is " << perimeter;
+    cout << "\nThe Area of a " << length << "ft long by " << width << "ft wide room is " << area << " square feet\n";
+
+    return 0;
+}
