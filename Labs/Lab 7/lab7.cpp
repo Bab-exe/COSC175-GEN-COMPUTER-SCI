debee5ada1445ef18f0730c92496bedf16ab47c2
Blessing <96212794+Bab-exe@users.noreply.github.com>
2022-10-23 20:24:10 -0400

    Lab 7 added
    Lab 7 coursework not submitted

---
+++ Labs/Lab 7/lab7.cpp
@@ -0,0 +1,22 @@
+/*
+    Blessing Abumere
+    COSC 175
+    10/19/2022
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num;
+ 
+	cout << "Counting " << endl;
+	for (num = 0; num < 5; num++)
+	{
+		cout << num << endl;		 
+	}
+	cerr << "final value of num is " << num << endl;
+	return 0;
+}
