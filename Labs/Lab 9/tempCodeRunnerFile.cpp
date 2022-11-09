cb638a4662ce26ffae198b7c067c5f9413f8eec0
Blessing <96212794+Bab-exe@users.noreply.github.com>
2022-11-09 10:04:35 -0500

    Lab 9 Completed
    The Files that were submitted for the assignment for lab 9

---
+++ Labs/Lab 9/tempCodeRunnerFile.cpp
@@ -0,0 +1,40 @@
+ /*
+    Blessing Abumere
+    COSC 175
+    11/2/2022
+*/
+ 
+ //*********************************************
+//New Welcome Home Program
+//This program prints a "Welcome Home" message
+//*********************************************
+#include <iostream>
+using namespace std;
+
+        
+void PrintLines(int numLines); //prototype //Formal Parameters 
+
+int main()
+{
+	PrintLines(2); //Actual Parameters 
+	cout << "Welcome Home!" << endl;
+
+    PrintLines(4); //Actual Parameters 
+	return 0;
+
+} // end of main method
+
+
+//PrintLines prints lines of astericks, where
+//numlines specifiles how many lines to print
+//*********************************************
+void PrintLines(int numLines) //Formal Parameters 
+{
+	int line;
+
+	for (line = 0; line < numLines; line++)
+	{
+		cout << "******************************" << endl;
+	}
+
+}
