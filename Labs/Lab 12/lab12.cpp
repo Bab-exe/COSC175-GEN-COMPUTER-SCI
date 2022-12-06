28606cc9f38653f7d99f55aef08a02a92251d385
Blessing <96212794+Bab-exe@users.noreply.github.com>
2022-12-06 09:02:22 -0500

    Lab 12
    Lab 12 was completed in class

---
+++ Labs/Lab 12/lab12.cpp
@@ -0,0 +1,49 @@
+/*
+    Blessing Abumere
+    COSC 175
+    11/30/2022
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	char TicTacTow[3][3];
+
+	int  row;
+	int  col;
+	for (row = 0; row < 3; row++)
+	{
+		for (col = 0; col < 3; col++)
+			TicTacTow[row][col] = '?';
+	}
+
+	//2nd: Assign X to the First row and column
+    TicTacTow[0][0] = 'X';
+	
+
+	//3rd: Assign O to the Last row and column
+    TicTacTow[2][2] = 'O';
+
+
+
+
+	//1st: Print the array in table form
+
+	int r;
+	int c;
+
+	for (r = 0; r < 3; r++)
+	{
+		for (c = 0; c < 3; c++)
+			cout << TicTacTow[r][c] << " ";
+		cout << endl;
+	}
+
+
+
+	return 0;
+
+
+}
